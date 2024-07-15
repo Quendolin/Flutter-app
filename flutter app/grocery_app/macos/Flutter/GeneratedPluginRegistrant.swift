c6f83425d4881d135af9798fb8ab81cd4ee7c2a1
Quendolin <97144002+Quendolin@users.noreply.github.com>
2024-07-16 01:18:23 +0200

    e

--- flutter app/grocery_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter app/grocery_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
+import speech_to_text_macos
 import sqflite
 import url_launcher_macos
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
