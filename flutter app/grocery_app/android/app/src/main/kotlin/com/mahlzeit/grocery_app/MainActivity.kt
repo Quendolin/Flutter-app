3082920a820db280d382b647be411866d5380102
Quendolin <97144002+Quendolin@users.noreply.github.com>
2024-08-10 15:03:11 +0200

    d

--- flutter app/grocery_app/android/app/src/main/kotlin/com/mahlzeit/grocery_app/MainActivity.kt
+++ flutter app/grocery_app/android/app/src/main/kotlin/com/mahlzeit/grocery_app/MainActivity.kt
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 package com.mahlzeit.groceryapp
-=======
-package com.mahlzeit.grocery_app
->>>>>>> f1d2a277e0ffa7652f27579c330438f5c9d2c66d
+
 
 import io.flutter.embedding.android.FlutterActivity
 
